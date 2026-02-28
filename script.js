5c770c4cb35f0bf5af4ce33593294ca5c3bac375
Sebastian Bara <sebastianbara24@gmail.com>
2026-02-28 12:56:19 +0100

    Added more files for structure; added some img; added first menu_template and menu_style.css

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+//Sticky Warenkorb
+//Warenkorb in Responsive als Overlay mit Button zum Öffnen unten
+//Gerichte Widgets mit onclick --> Warenkorb
+//Content Wrapper mit max-width und margin auto
+
+//Funktionen:
+//Gericht hinzufügen
+
+function addToCart(dishId) {
+    
+}
+  //Array mit Gericht, Preis, Anzahl
+  //Zwischenvariable oder Array
+//Gericht Anzahl erhöhen und senken
+  //Push - Splice
+//Gericht entfernen - Delete Function
+//Gesamtpreis berechnen
+//Bestellung abschicken (Dialog mit Bestätigung)
+//init zum Laden der Gerichte
+
